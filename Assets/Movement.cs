801d1e31008ce79f8fb282f1a17ea34c2e252369
MyungKun Chang <58208787+CDDing@users.noreply.github.com>
2024-06-14 16:35:07 +0900

    function added

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    int clicked=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,8 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A)){
+            print(++clicked);
+        }
     }
 }
